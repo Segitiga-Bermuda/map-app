6783bdb7590812daae224bca6d272f03615f39df
GodsEnWrath <fastyazid@gmail.com>
2019-11-22 16:02:50 +0700

    add Countries.js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class Countries extends Component {
-    render() {
-        return (
-            <div>
+  constructor(props) {
+    super(props);
 
-            </div>
-        )
-    }
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response => {
+        this.setState({ data: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.data.length > 0 &&
+          this.state.data.map(({ name, flag }, key) => {
+            return (
+              <Card className="text-center" style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={flag} />
+                <Card.Body>
+                  <Card.Title>{name}</Card.Title>
+                  <Button variant="primary"><Link to={`/countries/${name.replace(/\s/g, '-').toLowerCase()}`}>Go somewhere</Link></Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+      </div>
+    );
+  }
 }
