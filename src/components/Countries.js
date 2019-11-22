e19b3fc94e93a5dfc4fc0aeb616c2fceb3931035
GodsEnWrath <fastyazid@gmail.com>
2019-11-22 16:37:20 +0700

    Country list showing

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 
 export default class Countries extends Component {
   constructor(props) {
@@ -24,15 +25,36 @@ export default class Countries extends Component {
 
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around"
+        }}
+      >
         {this.state.data.length > 0 &&
           this.state.data.map(({ name, flag }, key) => {
             return (
               <Card className="text-center" style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={flag} />
+                <Card.Img
+                  style={{
+                    height: "200px",
+                    justifyContent: "space-around"
+                  }}
+                  variant="top"
+                  src={flag}
+                />
                 <Card.Body>
                   <Card.Title>{name}</Card.Title>
-                  <Button variant="primary"><Link to={`/countries/${name.replace(/\s/g, '-').toLowerCase()}`}>Go somewhere</Link></Button>
+                  <Button variant="outline-primary">
+                    <Link
+                      to={`/countries/${name
+                        .replace(/\s/g, "-")
+                        .toLowerCase()}`}
+                    >
+                      More Details
+                    </Link>
+                  </Button>
                 </Card.Body>
               </Card>
             );
