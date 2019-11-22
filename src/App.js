99b48d68cf33c0d163a9b70a30afe36a455e6502
mei ashari <mei.ashari@gmail.com>
2019-11-22 16:57:25 +0700

    home ulang

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Home from "./components/Home"
-import AboutUs from "./components/AboutUs"
+//import AboutUs from "./components/AboutUs"
 import Footer from "./components/Footer"
-import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Home />
-      <AboutUs />
+      {/* <AboutUs /> */}
       <Footer />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
+//import Jumbotron from 'react-bootstrap/Jumbotron'
+import map from '../assets/images/map.jpg'
+import Image from 'react-bootstrap/Image'
 
 export default function Home() {
+    
     return (
         <div>
-            <p>HELLO</p>
+            {/* <Jumbotron> */}
+                <h1 style={{textAlign:"center"}}>M A P</h1>
+
+                    <Image style={{backgroundSize:"cover"}} src={map} fluid />
+
+                    
+  
+            {/* </Jumbotron> */}
         </div>
     )
 }
